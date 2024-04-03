5a8d5852cddbeb4d8877d4d096f64e15f49e0b21
Anthony Jesus Retana <anthony.retana@kuarctech.com>
2024-04-03 14:52:19 -0600

    Sorting Performances

--- src/app/project-manager/pages/project-management-dossier/project-management-dossier.component.ts
+++ src/app/project-manager/pages/project-management-dossier/project-management-dossier.component.ts
@@ -17,6 +17,7 @@ export class ProjectManagementDossierComponent implements OnDestroy {
   showOptions: boolean = false;
   performancesFreeProfesional: any[] = [];
   performancesBuys: any[] = [];
+  allPerformances: any[] = [];
 
   constructor(private router: Router, private store: Store, private formBuilder: FormBuilder, private osdDataService: OSDDataService) {
     this.formProjectManager = this.createForm();
@@ -37,9 +38,49 @@ export class ProjectManagementDossierComponent implements OnDestroy {
       this.performancesBuys = performancesBuy;
       
     });
-    console.log('Lista de performancesFreeProfesional', this.performancesFreeProfesional)
-    console.log('Lista de performancesBuys', this.performancesBuys)
+    setTimeout(() => {
+      let normalizedBuys = this.performancesBuys.map(buy => ({
+        Id: buy.Id,
+        Date: buy.Date,
+        Type: 'Compra',
+        JustifyingDocument: buy.JustifyingDocument,
+        Summary: buy.Summary
+      }));
+
+      this.allPerformances = [...this.performancesFreeProfesional, ...normalizedBuys];
+      this.sortDateLowestHighest(false);
+    }, 1525);
   }
+
+
+  sortDateLowestHighest(ascending: boolean = true) {
+    return this.allPerformances.sort((a, b) => {
+      let dateA = new Date(a.Date);
+      let dateB = new Date(b.Date);
+      return ascending ? dateA.getTime() - dateB.getTime() : dateB.getTime() - dateA.getTime();
+    });
+  }
+
+  sortBySummary(ascending: boolean = true) {
+    return this.allPerformances.sort((a, b) => {
+        let summaryA = a.Summary.toLowerCase();
+        let summaryB = b.Summary.toLowerCase();
+
+        if (summaryA < summaryB) return ascending ? -1 : 1;
+        if (summaryA > summaryB) return ascending ? 1 : -1;
+        return 0;
+    });
+  }
+  sortByType(ascending: boolean = true) {
+    return this.allPerformances.sort((a, b) => {
+        let typeA = a.Type.toLowerCase();
+        let typeB = b.Type.toLowerCase();
+
+        if (typeA < typeB) return ascending ? -1 : 1;
+        if (typeA > typeB) return ascending ? 1 : -1;
+        return 0;
+    });
+}
 
   ngOnDestroy(): void {
     setTimeout(() => {
